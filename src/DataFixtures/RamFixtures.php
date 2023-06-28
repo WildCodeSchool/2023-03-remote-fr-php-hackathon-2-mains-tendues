2a71c335269731296a07b853e60b020fe86b39d3
valphi41 <philippeau.valentin@gmail.com>
2023-06-28 18:53:08 +0200

    ajout ddes entités RAM et Stockage ainsi que de leurs fixturesÃ

---
+++ src/DataFixtures/RamFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ram;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class RamFixtures extends Fixture
+{
+    public const RAM = [
+        1,
+        2,
+        3,
+        4,
+        6,
+        8,
+        12,
+        16
+    ];
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::RAM as $size) {
+            $ram = new Ram();
+            $ram->setSize($size);
+            $manager->persist($ram);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Ram.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RamRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RamRepository::class)]
+class Ram
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $size = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function setSize(int $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+}
