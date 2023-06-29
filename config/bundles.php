92f3d669e9ad23d87e7f384bf65aa01fc0f4ddc7
Nina <nina.schadel@gmail.com>
2023-06-29 12:07:59 +0200

    page catalogue presque terminée, il manque les filtres et l'envoi de mail quand l'user clique sur le bouton radio

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\Typed\TypedBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
